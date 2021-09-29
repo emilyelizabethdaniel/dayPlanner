8b96bd5b8d0fca41f3021790a9c68e5654b0d879
Basquiat <ememdaniel98@gmail.com>
2021-09-28 22:05:15 -0500

    suck it suckers I got the classes to attach, added ids to every text box, made a fucntion for every text box to check if it's before, during or after, its ugly but it works

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,7 +2,16 @@
 
 var saveBtn = $("[class=saveBtn]");
 var allNotes = [];
-var input = $("input");
+var nine = $("#nine");
+var ten = $("#ten");
+var eleven = $("#eleven");
+var twelve = $("#twelve");
+var one = $("#one");
+var two = $("#two");
+var three = $("#three");
+var four = $("#four");
+var five = $("#five");
+
 
 saveBtn.on('click', function() {
     console.log($(this));
@@ -31,36 +40,154 @@ saveBtn.on('click', function() {
 
 var now = moment();
 var currentHour = moment().startOf('day');
-var updateCurrentHour = 13;
-console.log(currentHour);
+var updateCurrentHour = 9;
 $("span").text(now.format("MMMM DD, YYYY, hh:mm "));
 currentHour = moment(currentHour).add(updateCurrentHour, 'h');
-console.log(currentHour);
 
-
-function compareTimes() {
-    if (moment(now).isAfter(currentHour)) {
-        console.log("after!");
-        input.addClass("future");
-
-    } else if (moment(now).isBefore(currentHour)) {
-        console.log("before!");
-        input.addClass("past");
-    } else if (moment(now).isSame(currentHour)) {
-        input.addClass("present")
-        console.log("same!");
-    }
-    // updateHour();
-    // console.log(updateCurrentHour)
-};
 
 function updateHour() {
     updateCurrentHour = updateCurrentHour + 1;
     console.log(updateCurrentHour);
 };
 
-compareTimes();
-updateHour();
+function compareTimesNine() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        nine.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        nine.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        nine.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+
+function compareTimesTen() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        ten.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        ten.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        ten.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesEleven() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        eleven.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        eleven.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        eleven.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesTwelve() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        twelve.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        twelve.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        twelve.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesOne() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        one.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        one.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        one.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesTwo() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        two.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        two.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        two.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesThree() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        three.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        three.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        three.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesFour() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        four.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        four.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        four.addClass("present")
+        console.log("same!");
+    }
+    updateHour();
+};
+
+function compareTimesFive() {
+    if (moment(now).isAfter(currentHour)) {
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before!");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        five.addClass("present")
+        console.log("same!");
+    }
+};
+
+
+compareTimesNine();
+compareTimesTen();
+compareTimesEleven();
+compareTimesTwelve();
+compareTimesOne();
+compareTimesTwo();
+compareTimesThree();
+compareTimesFour();
+compareTimesFive();
+
+
 
 
 // var timeObj = moment(fakeTime);
